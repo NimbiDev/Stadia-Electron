ee05741a7d3fa7aadbcfd8db40add75ef2bcb795
Nimbi <JoshuaVL1988@protonmail.com>
2022-06-28 18:57:41 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,54 @@
+// Modules to control application life and create native browser window
+const { app } = require("electron");
+const path = require("path");
+const { menubar } = require("menubar");
+
+const mb = menubar({
+  browserWindow: {
+    width: 1280,
+    height: 768,
+    resizable: true,
+    movable: true,
+    minimizable: true,
+    maximizable: true,
+    center: true,
+    closable: true,
+    fullscreen: false,
+    fullscreenable: true,
+    hasShadow: true,
+    autoHideMenuBar: true,
+    frame: true
+  },
+  preloadWindow: true,
+  icon: path.join(__dirname, "./MenuIcon.png"),
+  webPreferences: {
+    partition: "persist:googlestadia",
+  },
+});
+
+
+mb.app.commandLine.appendSwitch(
+  "disable-backgrounding-occluded-windows",
+  "true"
+);
+
+mb.on("ready", () => {
+  console.log("app is ready");
+
+  win = mb.window;
+  // win.openDevTools();
+
+  // First URL
+  win.loadURL("https://stadia.google.com/");
+
+});
+
+// mb.on('after-create-window', () => {}
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
